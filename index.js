efec309cd3415cb7ad98f77b10e099769644b381
yooge <56222194@qq.com>
2020-11-27 03:30:12 +0800

    补充

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var install_version = require('./install.version.js');
 //var patch = require('./install.patch.js');
 
 var deploy = require('./deploy.js');
+var installapk = require('./install_apk.js');
 module.exports = {
 	robot,
     version: install_version,
     //patch,
-    deploy
+    deploy,
+    installapk
 };
