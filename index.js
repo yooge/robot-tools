6d9b1c45cdcd5623d8ecafe595c4a82dc5424302
yooge <56222194@qq.com>
2020-11-27 17:34:33 +0800

    pc的脚本去掉引用  1.0.20

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ var robot = require('./robots.js');
 var install_version = require('./install.version.js');
 //var patch = require('./install.patch.js');
 
-var deploy = require('./deploy.js');
-var installapk = require('./install_apk.js');
 module.exports = {
 	robot,
     version: install_version,
-    //patch,
-    deploy,
-    installapk
+    //patch, 
 };
